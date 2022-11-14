4064a05896d6b03f24a29af220afb5c49e122440
greyqwilson <98433648+greyqwilson@users.noreply.github.com>
2022-11-14 13:25:24 -0800

    added various plotting examples

--- activity4.py
+++ activity4.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 def main():
+    #Data consists of only FM-15 data and all '9999' entries have been removed
     data = pd.read_csv("ontarioAirport.csv")
     #make sure data time does not match, so no duplicates
     tempSeries = data['TMP']
@@ -39,8 +40,6 @@ def main():
     y_2020 = sampleList[131414:140174]
     y_2021 = sampleList[140175:148935]
     y_2022 = sampleList[148927:156502]
-
-    listOfYears = []
     
     hourRange = []
     #Make complementary hour list for data array
@@ -58,12 +57,59 @@ def main():
                                   y_2015, y_2016, y_2017, y_2018, y_2019,
                                   y_2020, y_2021]).transpose()
    # print(sample)
-    sampleTemp = pd.DataFrame(sample, columns=['hours', '2005', '2006', '2007',
+    tempData = pd.DataFrame(sample, columns=['hours', '2005', '2006', '2007',
                                                 '2008', '2009', '2010', '2011',
                                                 '2012', '2013', '2014', '2015',
                                                 '2016', '2017', '2018', '2019', 
                                                 '2020', '2021'])
-    a1 = sampleTemp.plot.line(x='hours', y='2005', c='DarkBlue')
-    a2 = sampleTemp.plot.line(x='hours', y='2006', color='Green')
+    figure, hoursAxis = plt.subplots()
+
+    #Manually set ticks to line up with months (ticks are in hours)
+    hoursAxis.set_xticks([0, 730, 1460, 2190, 2920, 3650, 4380, 5110, 5840, 6570, 7300, 8030, 8760])
+    hoursAxis.set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec', 'Jan'])
+    hoursAxis2 = hoursAxis.twinx()
+    
+    #SIMPLE LINE PLOT
+
+    # tempData.plot.line(x='hours', y='2005', color='#c0d2ff', ax=hoursAxis)
+    # tempData.plot.line(x='hours', y='2006', color='#b6cbff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2007', color='#adc4ff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2008', color='#a0bbff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2009', color='#96b4ff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2010', color='#86a8ff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2011', color='#7a9fff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2012', color='#6e96ff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2013', color='#648fff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2014', color='#5685ff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2015', color='#467aff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2016', color='#3f75ff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2017', color='#316bff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2018', color='#2965ff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2019', color='#205eff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2020', color='#1556ff', ax=hoursAxis2)
+    # tempData.plot.line(x='hours', y='2021', color='#0b4eff', ax=hoursAxis2) 
+
+
+    #Attempt at iterating graphs instead of manually entering
+    # yearString = ''
+    # for i in range(2006, 2022):
+    #     yearString = str(i)
+    #     tempData.plot.line(x='hours', y=yearString, color='#b6cbff', ax=hoursAxis2)
+    
+    #HISTOGRAM with fit line
+    #tempBins = [0, 2.5, 5, 7.5, 10, 12.5, 15, 17.5, 20, 22.5, 25, 27.5, 30, 32.5, 35, 37.5, 40]
+    #plt.hist(tempData['2005'], alpha=1, color='#1000ff', label='2005', bins=tempBins)
+    #plt.hist(tempData['2021'], alpha=1, color='#466fdc', label='2021', bins=tempBins)
+    #plt.hist(tempData['2007'], alpha=1, color='#6eDDff', label='2007', bins=tempBins)
+
+    #figure.tight_layout()
+    plt.legend()
+
+    
+    plt.hot()
+
+    #Mean temperature
+    
+    
     plt.show()
 main()
